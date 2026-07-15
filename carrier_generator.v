b9e0609b9a493730dd2b1cef0557b4e4265af729
Hammad <82674789+Hammad124707@users.noreply.github.com>
2026-07-15 10:07:04 +0500

    Implement main carrier module with phase accumulator

--- carrier_generator.v
+++ carrier_generator.v
@@ -1,1 +1,35 @@
+module main_carrier (clk,reset,wave);
+
+input clk ;
+input reset ;
+output [11:0] wave ;
+localparam step = 32'd1073741824 ;
+reg [31:0] phase_accumulator ;
+reg wave ;
+wire sum = phase_accumulator + step ;
+wire lut_bits = phase_accumulator[31:30] ;
+
+always@(posedge clk ) 
+begin 
+if(reset) begin 
+phase_accumulator <= 0 ;
+wave <= 0 ;
+end 
+
+else
+phase_accumulator <= sum ;
+
+end
+
+always@(*)
+case(lut_bits)
+2'b00 : wave <= 12'd0;
+2'b01: wave <= 12'sd2048 ;
+2'b10 : wave <= 12'sd0 ;
+2'b11: wave <= -12'sd2048;
+endcase
+endmodule
+
+
+
 
